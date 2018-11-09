52a121ba1dbbab29cb03b241d738c367928e9074
João Sousa <jcs@lasige.di.fc.ul.pt>
2018-11-09 00:09:59 +0000

    Created new class bftsmart.tom.server.defaultservices.blockchain.BlockchainRecoverable. Most of the people still need to be implemented, but it already provides an extra method to validate a batch against its respective consensus proof.

---
+++ src/bftsmart/tom/server/defaultservices/blockchain/BlockchainRecoverable.java
@@ -0,0 +1,189 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bftsmart.tom.server.defaultservices.blockchain;
+
+import bftsmart.consensus.messages.ConsensusMessage;
+import bftsmart.reconfiguration.ServerViewController;
+import bftsmart.reconfiguration.util.TOMConfiguration;
+import bftsmart.statemanagement.ApplicationState;
+import bftsmart.statemanagement.StateManager;
+import bftsmart.statemanagement.strategy.StandardStateManager;
+import bftsmart.tom.MessageContext;
+import bftsmart.tom.ReplicaContext;
+import bftsmart.tom.core.messages.TOMMessage;
+import bftsmart.tom.server.BatchExecutable;
+import bftsmart.tom.server.Recoverable;
+import bftsmart.tom.util.BatchBuilder;
+import bftsmart.tom.util.TOMUtil;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.security.PublicKey;
+import java.util.Arrays;
+import java.util.HashSet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author joao
+ */
+public abstract class BlockchainRecoverable implements Recoverable, BatchExecutable {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+    
+    private TOMConfiguration config;
+    private ServerViewController controller;
+    private StateManager stateManager;
+    
+    @Override
+    public void setReplicaContext(ReplicaContext replicaContext) {
+        this.config = replicaContext.getStaticConfiguration();
+        this.controller = replicaContext.getSVController();
+        //initLog();
+        getStateManager().askCurrentConsensusId();
+    }
+
+    @Override
+    public ApplicationState getState(int cid, boolean sendState) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int setState(ApplicationState state) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public StateManager getStateManager() {
+        if (stateManager == null) {
+            stateManager = new StandardStateManager(); // might need to be other implementation
+        }
+        return stateManager;
+    }
+
+    @Override
+    public void Op(int CID, byte[] requests, MessageContext msgCtx) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void noOp(int CID, byte[][] operations, MessageContext[] msgCtx) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public byte[][] executeBatch(byte[][] command, MessageContext[] msgCtx) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public byte[] executeUnordered(byte[] command, MessageContext msgCtx) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public byte[] takeCheckpointHash() {
+        return TOMUtil.computeHash(getSnapshot());
+    }
+    
+    public boolean verifyBatch(byte[][] commands, MessageContext[] msgCtxs){
+        
+        //first off, re-create the batch received in the PROPOSE message of the consensus instance
+        int totalMsgsSize = 0;
+        byte[][] messages = new byte[commands.length][];
+        byte[][] signatures = new byte[commands.length][];
+        
+        for (int i = 0; i <commands.length; i++) {
+            
+            TOMMessage msg = msgCtxs[i].recreateTOMMessage(commands[i]);
+            messages[i] = msg.serializedMessage;
+            signatures[i] = msg.serializedMessageSignature;
+            totalMsgsSize += msg.serializedMessage.length;
+        }
+        
+        BatchBuilder builder = new BatchBuilder(0);
+        byte[] serializeddBatch = builder.createBatch(msgCtxs[0].getTimestamp(), msgCtxs[0].getNumOfNonces(), msgCtxs[0].getSeed(),
+                commands.length, totalMsgsSize, true, messages, signatures);
+        
+        // now we can obtain the hash contained in the ACCEPT messages from the proposed value
+        byte[] hashedBatch = TOMUtil.computeHash(serializeddBatch);
+        
+        //we are now ready to verify each message that comprises the proof
+        int countValid = 0;
+        int certificate = (2*controller.getCurrentViewF()) + 1;
+        
+        HashSet<Integer> alreadyCounted = new HashSet<>();
+        
+        for (ConsensusMessage consMsg : msgCtxs[0].getProof()) {
+            
+            ConsensusMessage cm = new ConsensusMessage(consMsg.getType(),consMsg.getNumber(),
+                    consMsg.getEpoch(), consMsg.getSender(), consMsg.getValue());
+            
+            cm.setCheckpointHash(consMsg.getCheckpointHash());
+
+            ByteArrayOutputStream bOut = new ByteArrayOutputStream(248);
+            try {
+                new ObjectOutputStream(bOut).writeObject(cm);
+            } catch (IOException ex) {
+                logger.error("Could not serialize message",ex);
+            }
+
+            byte[] data = bOut.toByteArray();
+                        
+            PublicKey pubKey = config.getPublicKey(consMsg.getSender());
+
+            byte[] signature = (byte[]) consMsg.getProof();
+
+            if (Arrays.equals(consMsg.getValue(), hashedBatch) &&
+                    TOMUtil.verifySignature(pubKey, data, signature) && !alreadyCounted.contains(consMsg.getSender())) {
+
+                alreadyCounted.add(consMsg.getSender());
+                countValid++;
+            } else {
+                logger.error("Invalid signature in message from " + consMsg.getSender());
+            }
+        }
+        
+        boolean ret = countValid >=  certificate;
+        logger.info("Proof for CID {} is {} ({} valid messages, needed {})",
+                msgCtxs[0].getConsensusId(), (ret ? "valid" : "invalid"), countValid, certificate);
+        return ret;
+    }
+ 
+    /**
+     * Given a snapshot received from the state transfer protocol, install it
+     * @param state The serialized snapshot
+     */
+    public abstract void installSnapshot(byte[] state);
+    
+    /**
+     * Returns a serialized snapshot of the application state
+     * @return A serialized snapshot of the application state
+     */
+    public abstract byte[] getSnapshot();
+    
+    /**
+     * Execute a batch of ordered requests
+     * 
+     * @param commands The batch of requests
+     * @param msgCtxs The context associated to each request
+     * @param fromConsensus true if the request arrived from a consensus execution, false if it arrives from the state transfer protocol
+     * 
+     * @return the respective replies for each request
+     */
+    public abstract byte[][] appExecuteBatch(byte[][] commands, MessageContext[] msgCtxs, boolean fromConsensus);
+    
+    /**
+     * Execute an unordered request
+     * 
+     * @param command The unordered request
+     * @param msgCtx The context associated to the request
+     * 
+     * @return the reply for the request issued by the client
+     */
+    public abstract byte[] appExecuteUnordered(byte[] command, MessageContext msgCtx);
+}
